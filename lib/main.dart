f5d21857b9974b4c88d0c2cf7c2da76ce20dc318
MSJ2025 <msj2025@icloud.com>
2025-07-12 00:31:03 +0200

    Supprimer le test d\'accès réseau

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
-import 'dart:io';
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'utils/theme.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await _testLocalNetworkAccess();
   runApp(const OPJFichesApp());
-}
-
-Future<void> _testLocalNetworkAccess() async {
-  try {
-    final socket = await Socket.connect('192.168.1.1', 80, timeout: Duration(seconds: 2));
-    socket.destroy();
-  } catch (_) {
-    // Erreur ignorée intentionnellement
-  }
 }
 
 class OPJFichesApp extends StatelessWidget {
