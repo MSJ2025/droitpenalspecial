084eb513a5fa4aa33fab0f092cdc49831553676e
MSJ2025 <msj2025@icloud.com>
2025-07-13 17:31:11 +0200

    Amélioration UI : encapsulation des cartes classement dans une carte moderne

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.poljud"
+    namespace = "com.msj2025.pjplus"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
