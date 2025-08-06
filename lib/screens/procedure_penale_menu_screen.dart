5621321581fa588dfe58e8dab9190352a8ad7a85
MSJ2025 <msj2025@icloud.com>
2025-08-06 10:42:08 +0200

    Supprime les cadres d'enquête

--- lib/screens/procedure_penale_menu_screen.dart
+++ lib/screens/procedure_penale_menu_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/adaptive_appbar_title.dart';
-import '../widgets/modern_gradient_button.dart';
 import '../widgets/ad_banner.dart';
-import 'cadre_list_screen.dart';
 
 
 class ProcedurePenaleMenuScreen extends StatelessWidget {
@@ -38,23 +36,6 @@ class ProcedurePenaleMenuScreen extends StatelessWidget {
                         child: Image.asset(
                           'assets/images/logocreme.png',
                           width: 260,
-                        ),
-                      ),
-                      FractionallySizedBox(
-                        widthFactor: 0.85,
-                        child: ModernGradientButton(
-                          icon: Icons.gavel,
-                          label: "Cadres d'enquête",
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              PageRouteBuilder(
-                                pageBuilder: (_, animation, __) => FadeTransition(
-                                  opacity: animation,
-                                  child: const CadreListScreen(),
-                                ),
-                              ),
-                            );
-                          },
                         ),
                       ),
                     ],
